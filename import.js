365d1d3d4098fc9b9bb5e3ecbf1f483fe8f5737e
Mohit Chandwani <154326745+mohitchandwai@users.noreply.github.com>
2025-07-01 16:14:22 +0530

    Delete import.js

--- import.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
-import DatePicker from 'react-datepicker';
-import NewsTicker from './components/News'; // Import the News components
-import 'react-datepicker/dist/react-datepicker.css';
-import './App.css'; // Import the CSS
